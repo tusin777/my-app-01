ca2fe62a6cace3bc58412292691fa6a87b2459e8
Алексей Рыбак <rybak2108@yandex.ru>
2025-03-01 18:04:13 +0300

    checked

--- src/components/Email.jsx
+++
@@ -1,7 +0,0 @@
-export function Email() {
-  const email = "alexei@gmail.com";
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const check = emailRegex.test(email);
-
-  return check ? email : null;
-}
