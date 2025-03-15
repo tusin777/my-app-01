54fdd7b80539180e466f088396454830d443fd0f
Алексей Рыбак <rybak2108@yandex.ru>
2025-03-15 19:57:13 +0300

    checked

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//import { DataFetcher } from "./DataFetcher";
 import App from "./App";
-import Cleanup from "./Cleanup";
-import WindowSize from "./WindowSize";
+import { RefComponents } from "./RefComponent";
+import { RefComponentEffect } from "./RefComponentEffect";
+import NumberTracker from "./NumberTracker";
+import UserComponent from "./UserComponent";
+import { CustomVideoPlayer } from "./CustomVideoPlayer";
+import AdvancedAudioPlayer from "./AdvancedAudioPlayer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <DataFetcher /> */}
     {/* <App /> */}
-    <Cleanup />
-    {/* <WindowSize /> */}
+    {/* <RefComponents /> */}
+    {/* <RefComponentEffect /> */}
+    {/* <NumberTracker /> */}
+    {/* <UserComponent /> */}
+    <CustomVideoPlayer />
+    {/* <AdvancedAudioPlayer /> */}
   </StrictMode>
 );
