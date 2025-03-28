b1981db502b50664fbfbe81d1cdfb86002ffb4a5
Алексей Рыбак <rybak2108@yandex.ru>
2025-03-28 23:53:33 +0300

    checked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,26 @@
-import { BasicExample } from "./BasicExample";
-import { FilterExample } from "./FilterExample";
-import { SlowRenderExample } from "./SlowRenderExample";
-import { TabExample } from "./TabExample";
-import { SyncTabExample } from "./SyncTabExapmle";
+import { useDeferredValue, useEffect, useState } from "react";
+import { SearchList } from "./SearchList";
 
-const App = () => {
+function App() {
+  const [query, setQuery] = useState("");
+  console.log("**************");
+  console.log("Текст в поле input:", query);
+  const deferredQuery = useDeferredValue(query);
+
+  useEffect(() => {
+    console.log("App useEffect query:", query);
+  }, [query]);
+
+  useEffect(() => {
+    console.log("App useEffect deferredQuery:", deferredQuery);
+  }, [deferredQuery]);
+
   return (
     <>
-      {/* <BasicExample /> */}
-      {/* <SlowRenderExample />*/}
-      {/* <FilterExample /> */}
-      <TabExample />
-      <SyncTabExample />
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <SearchList query={deferredQuery} />
     </>
   );
-};
+}
 
 export default App;
